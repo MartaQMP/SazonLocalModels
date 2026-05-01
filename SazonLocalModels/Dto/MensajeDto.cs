7eaa18ee5e108c30932ffb4602da926e8085c52c
MartaQMP <martaquirosmp@gmail.com>
2026-05-01 12:44:18 +0200

    Changes (mensajes)

--- SazonLocalModels/Dto/MensajeDto.cs
+++ SazonLocalModels/Dto/MensajeDto.cs
@@ -8,6 +8,8 @@ namespace SazonLocalModels.Dto
 {
     public class MensajeDto
     {
+        public string Nombre { get; set; }
+        public string Email { get; set; }
         public string TipoConsulta { get; set; }
         public string Asunto { get; set; }
         public string Contenido { get; set; }
